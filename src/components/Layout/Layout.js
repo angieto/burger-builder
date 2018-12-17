7f172da29708f5f7649f87e1a1dde117a7f2dc83
Angie To <angie.to@berkeley.edu>
2018-12-17 14:59:28 -0800

    layouted the project's structure

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Layout.module.css';
+
+const layout = (props) => (
+    <>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}> {props.children} </main>
+    </>
+);
+
+export default layout;
