7bc14853188944c025fa3c0810d3fae2b9007825
Angie To <angie.to@berkeley.edu>
2018-12-18 06:54:34 -0800

    created individual <BuildControl />, added style.css

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './BuildControl.module.css';
+
+const buildControl = (props) => (
+    <div className={classes.BuildControl}>
+        <div className={classes.Label}>{props.label}</div>
+        <button className={classes.Remove}>Remove</button>
+        <button className={classes.Add}>Add</button>
+    </div>
+);
+
+export default buildControl;
