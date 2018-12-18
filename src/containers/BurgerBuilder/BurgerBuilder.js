51e19df36b593ea9ce59e20be8c8925939cedf43
Angie To <angie.to@berkeley.edu>
2018-12-18 14:32:38 -0800

    implemented backdrop function

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -58,6 +58,9 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({ purchasing: true });
     }
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
 
     render() {
         const disabledInfo = {
@@ -68,7 +71,7 @@ class BurgerBuilder extends Component {
         }
         return (
             <>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
