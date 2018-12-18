b00f70cda57e5dd49e0491c94a0dda6d01dd18a2
Angie To <angie.to@berkeley.edu>
2018-12-18 08:29:05 -0800

    added functionality to build controls (add/remove event handlers)

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+// name a GLOBAL const in capital letters
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.75,
+    cheese: 0.5,
+    meat: 1.25
+}
+
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
@@ -9,14 +17,41 @@ class BurgerBuilder extends Component {
             bacon: 1,
             cheese: 1,
             meat: 1
-        }
+        },
+        totalPrice: 2.50
+    }
+
+    // methods
+    addIngredientHandler = (type) => {
+        const updatedIngredients = { ...this.state.ingredients };
+        const updatedCount = this.state.ingredients[type] + 1;
+        updatedIngredients[type] = updatedCount;
+        const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: updatedPrice
+        })
+    }
+    removeIngredientHandler = (type) => {
+        if (this.state.ingredients[type] <= 0) return;
+        const updatedIngredients = { ...this.state.ingredients };
+        const updatedCount = this.state.ingredients[type] - 1;
+        updatedIngredients[type] = updatedCount;
+        const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: updatedPrice
+        })
     }
 
     render() {
         return (
             <>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                />
             </>
         )
     }
