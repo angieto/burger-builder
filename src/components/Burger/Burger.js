08a4439a1a939e0f2a620567969c11341e890468
Angie To <angie.to@berkeley.edu>
2018-12-18 06:36:20 -0800

    added alert message to notify the user when the list of ingredients is empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -8,12 +8,22 @@ const burger = (props) => {
     // Array(num) creates a new array with num space
     // since the arugument itself is not used in this case, name it as "_"
     // we want the index of each props.ingredients[key] to generate a unique key to each ingredient
-    const transformedIngredients = Object.keys(props.ingredients)
+    // return an array of <BurgerIngredient key={} type={} />
+    // reduce(callback(prevVal, curVal), initial value), returns & adds to the previous value for each loop
+    // use reduce() to flatten the arr of Arrays to the arr of <BurgerIngredient/> and access its length
+    let transformedIngredients = Object.keys(props.ingredients)
                                          .map(key => {
                                              return [...Array(props.ingredients[key])].map( (_,i) => {
                                                  return <BurgerIngredient key={key+i} type={key} />
                                              }); 
-                                         });
+                                         })
+                                         .reduce( (arr, element) => {
+                                            return arr.concat(element);
+                                         }, [] );
+    // set up alert msg when the ingredient list is empty
+    if (!transformedIngredients.length) {
+        transformedIngredients = <p className="text-danger">I am an empty burger!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,10 @@ import Burger from '../../components/Burger/Burger';
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 2,
-            bacon: 1,
-            cheese: 1,
-            meat: 1
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
 
