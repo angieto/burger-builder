134e7ee99ec3553975064cf61a4ded3cf99a6db5
Angie To <angie.to@berkeley.edu>
2018-12-18 23:01:25 -0800

    reused <Backdrop /> in <SideDrawer />, converted stateless <layout /> into a stateful component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}> {props.children} </main>
-    </>
-);
+class Layout extends Component {
+    render() {
+        return (
+            <>
+                <Toolbar />
+                <SideDrawer />
+                <main className={classes.Content}> {this.props.children} </main>
+            </>
+        );
+    }
+};
+    
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,21 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const sideDrawer = (props) => {
     return(
-        <div className={classes.SideDrawer}>
-        <div className={classes.Logo}>
-            <Logo />
-        </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        <>
+            <div className={classes.SideDrawer}>
+                <Backdrop show={true}/>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
+            </div>
+        </>
     );
 };
 
