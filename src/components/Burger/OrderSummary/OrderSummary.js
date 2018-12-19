cc8101540f79686503bf20710751a2a4a79f725e
Angie To <angie.to@berkeley.edu>
2018-12-19 11:15:55 -0800

    added life-cycle hooks to <Modal /> for run time optimization

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map( key => {
             return (
                 <li key={key}>
-                    <strong className="text-capitalize">{key}</strong>: {props.ingredients[key]}
+                    <strong className="text-capitalize">{key}</strong>: {this.props.ingredients[key]}
                 </li>
             );
         });
-    return (
-        <>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                { ingredientSummary }
-            </ul>
-            <p><strong>Total: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>Cancel</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>Continue</Button>
-        </>
-    );
-};
+        return (
+            <>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    { ingredientSummary }
+                </ul>
+                <p><strong>Total: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>Cancel</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>Continue</Button>
+            </>
+        );
+    }
+}
+    
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React , { Component } from 'react';
 import classes from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-        <div className={classes.Modal} 
-            style={{transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'}}>
-            { props.children }
-        </div>
-    </>
-);
+class Modal extends Component {
+    // for run time optimization, only rerender when the purchase state (props) is changed
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
 
-export default modal;
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render() {
+        return (
+            <>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal} 
+                    style={{transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                            opacity: this.props.show ? '1' : '0'}}>
+                    { this.props.children }
+                </div>
+            </>
+        )
+    }
+}
+
+export default Modal;
