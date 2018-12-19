13523420685b9ab95ab6adbc5518090ab9752ffb
Angie To <angie.to@berkeley.edu>
2018-12-18 19:45:20 -0800

    added navigation bar, toolbar, and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <>
+        <Toolbar />
         <main className={classes.Content}> {props.children} </main>
     </>
 );
