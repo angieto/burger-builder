e988d5be1b49da208ccef24f0b4cddcdee579533
Angie To <angie.to@berkeley.edu>
2018-12-19 10:46:23 -0800

    added new file DrawerToggle.js to handle the toggle function in Toolbae

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,11 +12,16 @@ class Layout extends Component {
     sideDrawerClosedHandler = () => {
         this.setState({ showSideDrawer: false });
     }
+    sideDrawerToggleHandler = () => {
+        this.setState( (prevState) => { 
+            return { showSideDrawer: !prevState.showSideDrawer }; 
+        });
+    }
 
     render() {
         return (
             <>
-                <Toolbar />
+                <Toolbar clicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}> {this.props.children} </main>
             </>
