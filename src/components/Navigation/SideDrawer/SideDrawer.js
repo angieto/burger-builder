4d57b16dc0b0eeeb82f7ab740980ed6a5322f4d0
Angie To <angie.to@berkeley.edu>
2018-12-18 20:56:59 -0800

    adjusted the logo's position and height in sidedrawer and toolbar components

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,7 +6,9 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const sideDrawer = (props) => {
     return(
         <div className={classes.SideDrawer}>
+        <div className={classes.Logo}>
             <Logo />
+        </div>
             <nav>
                 <NavigationItems />
             </nav>
