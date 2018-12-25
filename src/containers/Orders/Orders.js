e7fe7760199efdc00a07b92947132dc2a233f23e
Angie To <angie.to@berkeley.edu>
2018-12-25 15:08:01 -0800

    fetched orders from backend and display orders on the page

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
+import ErrorHandler from '../../hoc/ErrorHandler/ErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+             .then(res => {
+                 // turn obj to an array
+                 const fetchedOrders = [];
+                 for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                 }
+                 this.setState({ orders: fetchedOrders, loading: false })
+             })
+             .catch(err => {
+                this.setState({ loading: false })
+             })
+    }
+
     render() {
         return (
             <div>
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={+order.price} />
+                ))}
             </div>
         );
     }
 }
 
-export default Orders;
+export default ErrorHandler(Orders, axios);
