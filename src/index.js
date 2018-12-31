40335a8341e4c8896e8468d950cb63d6192345fc
Angie To <angie.to@berkeley.edu>
2018-12-31 12:08:52 -0800

    added redux-devtool-extension to store

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware()));
 
 const app = (
     <Provider store={store}>
