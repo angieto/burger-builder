976d5a042d7145fa61e8642c074c0e7de432e24e
Angie To <angie.to@berkeley.edu>
2018-12-31 15:42:12 -0800

    refactored burgerbuilder reducer with utility

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     ingredients: null,
@@ -16,35 +17,29 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case (actionTypes.ADD_INGREDIENT):
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients, // one deeper level of cloning
-                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-                },
+            const updatedIngredient = { [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+            const updatedState = {
+                ingredients: updatedIngredients,
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             };
+            return updateObject(state, updatedState);
         case (actionTypes.REMOVE_INGREDIENT): 
-            return {
-                ...state,
-                ingredients: {
-                    ...state.ingredients, // one deeper level of cloning
-                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-                },
-                totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+            const updatedIng = { [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+            const updatedIngs = updateObject(state.ingredients, updatedIng);
+            const updatedSt = {
+                ingredients: updatedIngs,
+                totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             };
+            return updateObject(state, updatedSt);
         case (actionTypes.SET_INGREDIENTS):
-            return {
-                ...state,
+            return updateObject( state, {
                 ingredients: action.ingredients,
                 totalPrice: 2, // set price back to default
                 error: false // reset the error back to false
-            };
+            });  
         case (actionTypes.FETCH_INGREDIENTS_FAILED):
-            return {
-                ...state,
-                error: true
-            };
+            return updateObject(state, {error: true});
         default:
             return state;
     }
