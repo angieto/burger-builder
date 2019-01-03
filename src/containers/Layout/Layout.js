3ce7ca0578bf755d148298ace311132d11c4004f
Angie To <angie.to@berkeley.edu>
2019-01-02 20:30:21 -0800

    implemented logout feature, redirected to mainpage

--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -3,6 +3,8 @@ import classes from './Layout.module.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
+import { connect } from 'react-redux';
+
 class Layout extends Component {
     state = {
         showSideDrawer: true
@@ -21,13 +23,22 @@ class Layout extends Component {
     render() {
         return (
             <>
-                <Toolbar clicked={this.sideDrawerToggleHandler}/>
-                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <Toolbar
+                    isAuth={ this.props.isAuthenticated } 
+                    clicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer 
+                    isAuth={ this.props.isAuthenticated }
+                    open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}> {this.props.children} </main>
             </>
         );
     }
 };
     
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
 
-export default Layout;
+export default connect(mapStateToProps)(Layout);
